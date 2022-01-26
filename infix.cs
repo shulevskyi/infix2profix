c0e5cd3b9add8f6a7318aebbd846b5e51ed2b93b
istepmother <imachukha@kse.org.ua>
2022-01-26 09:35:41 +0200

    Weak calculation function

--- infix.cs
+++ infix.cs
@@ -7,7 +7,7 @@ public static class SampleStack
     {
         var stack = new Stack();
         var output = "";
-        
+
         int Precedence(char ch)
         {
             switch (ch)
@@ -23,12 +23,13 @@ public static class SampleStack
                 case '^':
                     return 3;
             }
+
             return -1;
         }
 
         string? InfixConverter(string data)
         {
-            
+
             // Getting rid of spaces in the string
             var trim = data.Replace(" ", "");
 
@@ -40,7 +41,7 @@ public static class SampleStack
                     {
                         // We put it to the stack, 'cause later we'll need to find it, in order to
                         // add elements to the output
-                        
+
                         case '(':
                         {
                             stack?.Push(t);
@@ -50,7 +51,7 @@ public static class SampleStack
                         {
                             // Loop that delete last elements from the stack and put it to the output,
                             // until ")" is found
-                            
+
                             while (stack is {Count: > 0} && (char) stack.Peek()! != '(')
                             {
                                 output += stack.Pop();
@@ -72,14 +73,14 @@ public static class SampleStack
                         }
                     }
                 }
-                
+
                 // If char is a digit - put it to the output
                 else
                 {
                     output += t;
                 }
             }
-            
+
             // Adding remaining values to the output
             while (stack is {Count: > 0})
             {
@@ -88,11 +89,49 @@ public static class SampleStack
 
             return output;
         }
-        
+
         // Usage of the function
-        var converted = InfixConverter("5 * (10 - 8) / 7 + 1");
+        var converted = InfixConverter("5 * 1 - 8 / 7 + 1");
         Console.WriteLine(converted);
+
+        // Count the result of the expression
+        var s = new Stack();
+        var operation = "";
+        var num1 = "";
+        var num2 = "";
+        string? Result(string expression)
+        {
+            var output = "";
+            foreach (var t in expression)
+            {
+                if (char.IsLetterOrDigit(t))
+                {
+                    s.Push(t);
+                }
+                else
+                {
+                    num1 += s.Pop();
+                    num2 += s.Pop();
+                    // need to make calculations with an operator
+                    operation  = num1 + t + num2;
+                    bool success = int.TryParse(operation, out _);
+                    if (success)
+                    {
+                        s.Push(operation);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Attempted conversion of '{operation}' failed.");
+                    }
+                }
+            }
+            output += s.Pop();
+            return output;
+        }
         
+        var calculated = Result(converted);
+        Console.WriteLine(calculated);
     }
-    
 }
+ 
+        
